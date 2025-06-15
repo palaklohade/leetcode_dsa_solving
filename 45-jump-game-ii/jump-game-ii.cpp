9dfa3fb7681878ffeec53203513f2f03c3bf0813
Palak Lohade <137188866+palaklohade@users.noreply.github.com>
2025-06-15 17:25:45 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (62.80%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int far = 0;
+        int jump = 0;
+        int end = 0 ;
+        for(int i = 0 ; i < nums.size() - 1;i++){
+            far = max(far , i + nums[i]);
+            if(i == end){
+                jump++;
+                end = far ;
+            }
+        }
+        return jump;
+    }
+};
