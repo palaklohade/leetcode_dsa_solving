3b754e7fa2f67e89ad01014bd8390494ced3db00
Palak Lohade <137188866+palaklohade@users.noreply.github.com>
2025-06-23 16:24:20 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (73.78%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -11,8 +11,33 @@
  */
 class Solution {
 public:
-    int countNodes(TreeNode* root) {
-        if(root == nullptr)return 0;
+    // int countNodes(TreeNode* root) {
+    //     if(root == nullptr)return 0;
+    //     return 1 + countNodes(root->left) + countNodes(root->right); 
+    int countNodes(TreeNode* root){
+        if(root == nullptr) return 0;
+        int left = getheight(root);
+        int right = getheightright(root);
+        if(left == right){
+            return (1 << left) - 1;
+        }
         return 1 + countNodes(root->left) + countNodes(root->right);
     }
+    int getheight(TreeNode* node){
+        int h = 0;
+        while(node){
+            h++;
+            node= node->left;
+        }
+        return h;
+    }
+    int getheightright(TreeNode* node){
+        int h = 0;
+        while(node){
+            h++;
+            node= node->right;
+        }
+        return h;
+    }
+    //this is the optimised soltion to be solved in o(log n ) time.
 };
