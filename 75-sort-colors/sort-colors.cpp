ca3c61552d3f75e3cd062f3946510718b39a3518
Palak Lohade <137188866+palaklohade@users.noreply.github.com>
2025-06-29 22:50:05 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (78.86%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,7 +1,20 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        
+        //sort(nums.begin(), nums.end());
+        //this will take o(n log n) time complexity 
+        //can it be more optimal ?
+        int low = 0; int high = nums.size() - 1;
+        int mid = 0;
+        while(mid <= high){
+            if(nums[mid] == 0){
+                swap(nums[low++], nums[mid++]);
+            }else if(nums[mid]== 1){
+                mid++;
+            }else{
+                swap(nums[mid],nums[high--]);
+            }
+        }
     }
+    //time complexity - o(n) optimised .
 };
