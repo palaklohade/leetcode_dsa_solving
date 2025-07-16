782974aef48278f898c3d8a36af8eede39232b97
Palak Lohade <137188866+palaklohade@users.noreply.github.com>
2025-07-16 20:25:11 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (6.29%) - LeetSync

---
+++ 274-h-index/h-index.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int h = 0;
+        sort(citations.rbegin(), citations.rend());
+        for(int i = 0 ; i < citations.size() ; i++){
+            if(citations[i] >= i + 1){
+                h = i + 1;
+            }else{
+                break;
+            }
+        }
+        return h;
+    }
+};
