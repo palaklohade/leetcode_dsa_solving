5d0b771b741204053659a24f5ea09d9cb692085b
Palak Lohade <137188866+palaklohade@users.noreply.github.com>
2025-07-18 18:55:57 +0530

    Time: 1 ms (2.73%) | Memory: 17.5 MB (86.57%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,41 +1,17 @@
 class Solution {
 public:
-
-    int firstocc(vector<int>& nums, int target){
-        int left = 0 ; int right = nums.size() - 1;
-        int ans = -1;
-
-        while(left <= right){
-            int mid = left + (right - left) / 2;
-            if(nums[mid] == target){
-                ans = mid;
-                right = mid  - 1;
-            }else if( nums[mid] < target){
-                left = mid +1;
-            }else{
-                right = mid - 1;
-            }
-        }
-        return ans;
-    }
-    int lastocc(vector<int>& nums, int target){
-        int left = 0 ; int right = nums.size() - 1;
-        int ans = -1;
-
-        while(left <= right){
-            int mid = left + (right - left) / 2;
-            if(nums[mid] == target){
-                ans = mid;
-                left = mid  + 1;
-            }else if( nums[mid] < target){
-                left = mid +1;
-            }else{
-                right = mid - 1;
-            }
-        }
-        return ans;
-    }
     vector<int> searchRange(vector<int>& nums, int target) {
-        return {firstocc(nums, target) , lastocc(nums,target)};
+        int n = nums.size();
+        int first = -1;
+        int last = -1;
+
+        for(int i = 0 ; i < n ; i++){
+            if(nums[i] == target){
+                if(first == -1){
+                    first = i;
+                }last = i;
+            }
+        }
+        return {first,last};
     }
 };
